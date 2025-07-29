0566725aecd72c0d6517c6762f3386d929ec92ad
Taniguchi Tomohiro <124371774+tomohiron907@users.noreply.github.com>
2025-07-29 22:38:05 +0900

    add software icon

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include "mainwindow.h"
 #include <QSurfaceFormat>
 #include <QVTKOpenGLNativeWidget.h>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    // アプリケーションアイコンを設定
+    app.setWindowIcon(QIcon(":/resources/strecs_icon.png"));
 
     // QVTKOpenGLNativeWidgetで使用するデフォルトのSurfaceFormatを設定
     QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
